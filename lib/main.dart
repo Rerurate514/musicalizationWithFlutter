88db08059cafcb17cf3f821a12922f7d8c9aaaa8
Rerurate514 <rerurateyuto@gmail.com>
2023-08-21 00:34:35 +0900

    ストレージへのアクセス権限要求

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+import 'permission.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: new ThemeData.dark(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -30,15 +32,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  PermissionRequest req = new PermissionRequest();
+  
+  ///このメソッドは、外部ストレージのパスを取得するメソッドです。
+  Future<String> getExternalStoragePath() async {
+    Directory? directory = await getExternalStorageDirectory();
+    
+    if(directory == null) {
+      print("Failed to access external storage in [getExternalStoragePath] method");
+      return "";
+    }
 
+    String path = directory.path;
+    return path;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'images/mp3_ui_mp3player_letters.png'
+        ),
+      ),
       body: Column(
         children: [
-          const Text("faga"),
-          const Text("fawfaf"),
+          
         ],
       )
     );
---
+++ lib/permission.dart
@@ -0,0 +1,20 @@
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class PermissionRequest{
+
+    /// このメソッドは、ストレージへのアクセス権限要求を行うメソッドです。
+    Future<void> requestDownloadStoragePermission() async {
+    PermissionStatus status = await Permission.storage.request();
+
+    if(status.isGranted){
+      print('Download permission granted.');
+    }
+    else{
+      print('Download permission denied.');
+    }
+  }
+
+
+}
