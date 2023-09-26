8761a94ade66c3be9a1d52f183789a273294df2e
Rerurate514 <rerurateyuto@gmail.com>
2023-09-26 17:13:55 +0900

    RealmIOResultsクラスの作成

---
+++ lib/logic/realm/logic/realmIOResults.dart
@@ -0,0 +1,19 @@
+class RealmIOResults<SCHEMA> {
+  late final bool _isSuccess;
+  get isSuccess => _isSuccess;
+
+  late final String _resultString;
+  get resultString => _resultString;
+
+  late final SCHEMA? _payload;
+  get payload => _payload;
+
+  RealmIOResults(
+      {required bool isSuccessArg,
+      SCHEMA? payloadArg,
+      String resultStringArg = ""}) {
+    _isSuccess = isSuccessArg;
+    _payload = payloadArg;
+    _resultString = resultStringArg;
+  }
+}
