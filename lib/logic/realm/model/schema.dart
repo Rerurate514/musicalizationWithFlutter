854f5d78ae01f5937b952db85f62b15ae69526e9
Rerurate514 <rerurateyuto@gmail.com>
2023-09-27 18:51:28 +0900

    カラムクラスのひな形作成

--- lib/logic/realm/model/schema.dart
+++ lib/logic/realm/model/schema.dart
@@ -1,21 +1,34 @@
 import 'package:realm/realm.dart';
 
+import '../logic/musicInfo/Column/musicName.dart';
+import '../logic/musicInfo/Column/musicPath.dart';
+import '../logic/musicInfo/Column/musicVolume.dart';
+import '../logic/musicInfo/Column/musicLyrics.dart';
+import '../logic/musicInfo/Column/musicPicture.dart';
+
+import '../logic/musicList/Column/listName.dart';
+import '../logic/musicList/Column/listMusicList.dart';
+
 part 'schema.g.dart';
+
+
+
 
 @RealmModel()
 class _MusicInfo{
   @PrimaryKey()
-  late ObjectId id;
-  late String name;
-  late double volume;
-  late String lyrics;
-  late String picture;
+  late ObjectId id;     //テーブルId
+  late MusicName name;     //曲名
+  late MusicPath path;     //曲のパス
+  late MusicVolume volume;   //音量
+  late MusicLyrics lyrics;   //歌詞
+  late MusicPicture picture;  //絵
 }
 
 @RealmModel()
 class _MusicList{
   @PrimaryKey()
-  late ObjectId id;
-  late String name;
-  late List<_MusicInfo> list;
+  late ObjectId id;           //テーブルId
+  late ListName name;           //リスト名
+  late ListMusicList list;   //MusicInfoのIDからなる曲リスト
 }
