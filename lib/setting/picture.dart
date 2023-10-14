1bf7096c3c6b0171f946518d028483ecbacc78c6
Rerurate514 <rerurateyuto@gmail.com>
2023-10-14 18:21:24 +0900

    ImageBtnに使用される画像の定数化

---
+++ lib/setting/picture.dart
@@ -0,0 +1,32 @@
+class PictureConstants{
+  String get appTitleLettersImg => 'images/mp3_ui_mp3player_letters.png';
+  String get musicRecordImg => 'images/mp3_menu_picture_setting.png'; 
+  String get shuffleImg => 'images/mp3_ui_music_shuffle_button.png';
+
+  //home
+  String get mainModeImg => 'images/mp3_ui_main_mode.png';
+  String get updateImg => 'images/mp3_ui_update.png';
+  String get fetchingFromGoogleDriveImg => 'images/mp3_ui_google_drive_button.png';
+
+  //list
+  String get listModeImg => 'images/mp3_ui_list_mode.png';
+  String get resisterMusicImg => 'images/mp3_ui_music_register_button.png';
+  String get musicAddToListImg => 'images/mp3_ui_list_add.png';
+  String get makeListImg => 'images/mp3_ui_list_make.png';
+
+
+  //play
+  String get playModeImg => 'images/mp3_ui_music_mode.png';
+  String get backMusicBtnImg => 'images/mp3_ui_back_music.png';
+  String get stopMusicBtnImg => 'images/mp3_ui_music_stop_button.png';
+  String get playMusicBtnImg => 'images/mp3_ui_play_button.png';
+  String get loopOffMusicBtnImg => 'images/mp3_ui_loop_button_off.png';
+  String get loopOnMusicBtnImg => 'images/mp3_ui_loop_button_on.png';
+  String get soundUnmuteOffMusicBtnImg => 'images/mp3_ui_sound_control_unmute_off.png';
+  String get soundUnmuteOnMusicBtnImg => 'images/mp3_ui_sound_control_unmute_on.png';
+  String get nextBtnMusicImg => 'images/mp3_ui_next_music.png';
+
+
+  //setting
+  String get settingModeImg => 'images/mp3_ui_setting_button.png';
+}
