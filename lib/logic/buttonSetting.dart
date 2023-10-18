b832279e5c4c0f4266b953ab86d8bedf63bbc8f9
Rerurate514 <rerurateyuto@gmail.com>
2023-10-18 12:53:52 +0900

    ButtonSettingクラスのgetterの作成

--- lib/logic/buttonSetting.dart
+++ lib/logic/buttonSetting.dart
@@ -1,6 +1,9 @@
 class ButtonSetting<BUTTONARGMENT>{
   late final String _btnPicture;
+  get btnPicture => _btnPicture;
+
   late final Function(BUTTONARGMENT) _btnFunction;
+  get btnFunction => _btnFunction;
 
   ButtonSetting(
     String btnPictureArg,
