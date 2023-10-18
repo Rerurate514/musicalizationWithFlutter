7c339aaeb02841619823bcc451de93b4bdf45fb2
Rerurate514 <rerurateyuto@gmail.com>
2023-10-18 11:59:35 +0900

    ListInListPageクラスのひな形作成

---
+++ lib/Pages/inListPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class InListPage extends StatefulWidget{
+  InListPage();
+
+  @override
+  State<InListPage> createState() => InListPageState();
+}
+
+class InListPageState extends State<InListPage>{
+  
+
+  @override
+  void initState(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return();
+  }
+}
