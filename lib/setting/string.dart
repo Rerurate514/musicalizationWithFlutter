a34ce6ebf35c0527b0d40374e4664e636157466c
Rerurate514 <rerurateyuto@gmail.com>
2023-10-18 20:12:06 +0900

    ドキュメント更新

--- lib/setting/string.dart
+++ lib/setting/string.dart
@@ -2,5 +2,11 @@ class StringConstants{
   String get appNameStr => "musicalization";
   String get shufflePlayButtonStr => "Shuffle play";
   
-  
+
+  String get listDialogTitle => "リスト名を決めてください"; 
+  String get listDialogTExtFieldHintText => "リスト名の入力"; 
+  String get listDialogOK => "OK";
+  String get listDialogCancel => "Cancel";
+  String get listDialogChoiceMusicTitle => "リストに追加する曲の選択";
+  String get listDialogCheckDeleteListTitle => "本当にこのリストを削除しますか？";
 }
